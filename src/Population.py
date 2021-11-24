503feab15cd7383003fdc2530cdb24596c6c2d06
aksek <kosekaniela@gmail.com>
2021-11-24 09:10:29 +0100

    basic algorithm structure, crossoverer

--- src/Population.py
+++ src/Population.py
@@ -4,6 +4,7 @@ import json
 from opfunu.cec.cec2013.unconstraint import Model
 from Mutator import Mutator
 from Generator import Generator
+from Crossoverer import Crossoverer
 
 class Population:
     
@@ -17,13 +18,33 @@ class Population:
         self.function = config['fun_number']
         self.mutator = Mutator(config['mutator'])
         self.generator = Generator(config['generator'])
+        #TODO: find a better name
+        self.crossoverer = Crossoverer(config['crossoverer'])
         self.population = []
         self.results = []
       
     
     def initialize(self):
         self.population = self.generator.generate_population()
-        
+
+    def run(self):
+        for epoch in range(self.epochs):
+            for i in range(len(self.population)):
+                individual = self.population[i]
+                random_1, random_2, random_3 = self.generate()
+                donor = self.mutator.mutate(random_1, random_2, random_3)
+                trial_vector = self.crossoverer.crossover(individual, donor)
+                if self.evaluate(trial_vector) <= self.evaluate(individual):
+                    self.population[i] = trial_vector
+            self.observeEpoch()
+
+
+# TODO: not implemented
+    def generate(self):
+        x1 = [1, 2, 3, 4, 5]
+        x2 = [1, 2, 3, 4, 5]
+        x3 = [1, 2, 3, 4, 5]
+        return x1, x2, x3
         
     def evaluate(self,x):
         return 1
