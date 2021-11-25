3a22c48660a865573c9fb71450a9366caf642f33
AMazur2 <arturmazur18@gmail.com>
2021-11-25 19:07:23 +0100

    Turn on observer, plot chart after simulation is over

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
 from src.Population import Population
+from src.Observer import Observer
 import numpy as np
 import pandas as pd
 
+
 def main():
     population = Population("config.json")
+    observer = Observer()
     population.run()
     results = population.getSimulationResults()
-    print(results)
-    #df = pd.DataFrame(data=np.array(results), )
+    df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
+    observer.plot_chart(df)
+
 
 if __name__ == '__main__':
     main()
--- src/Observer.py
+++ src/Observer.py
@@ -9,8 +9,8 @@ class Observer:
     def plot_chart(self, df):
         ax = plt.gca()
 
-        df.plot(kind='line', x='index', y='min', color='green', ax=ax)
-        df.plot(kind='line', x='index', y='average', color='red', ax=ax)
-        df.plot(kind='line', x='index', y='max', color='blue', ax=ax)
+        df.plot(kind='line', y='min', color='green', ax=ax)
+        df.plot(kind='line', y='average', color='red', ax=ax)
+        df.plot(kind='line', y='max', color='blue', ax=ax)
 
         plt.show()
