b85849ea8d166b22a28bc3defb118f7e1e7a2cef
AMazur2 <arturmazur18@gmail.com>
2021-11-25 19:57:29 +0100

    multiple configs run

--- main.py
+++ main.py
@@ -2,15 +2,21 @@ from src.Population import Population
 from src.Observer import Observer
 import numpy as np
 import pandas as pd
+import os
 
 
 def main():
-    population = Population("config.json")
-    observer = Observer()
-    population.run()
-    results = population.getSimulationResults()
-    df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
-    observer.plot_chart(df)
+    directory = os.getcwd()
+    for r, d, f in os.walk(directory + "/conf"):
+        f.sort()
+        for file in f:
+            print(file)
+            population = Population(directory + "/conf/" + file)
+            observer = Observer()
+            population.run()
+            results = population.getSimulationResults()
+            df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
+            observer.plot_chart(df)
 
 
 if __name__ == '__main__':
