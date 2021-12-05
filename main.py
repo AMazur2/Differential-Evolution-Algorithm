b66051253d5e825035b81c01471a218f67965705
AMazur2 <arturmazur18@gmail.com>
2021-12-05 21:04:54 +0100

    Many file run

--- main.py
+++ main.py
@@ -8,26 +8,35 @@ import os
 def main():
     runs = 12
     directory = os.getcwd()
-    resultsMin = []
-    resultsMax = []
-    resultsAvg = []
-    file = directory + "/conf/config2.json"
-    observer = Observer(file)
-    for i in range(runs):
-        population = Population(file)
-        population.run()
-        minimal, avg, maximal = population.getSimulationResults()
-        resultsMax.append(maximal)
-        resultsMin.append(minimal)
-        resultsAvg.append(avg)
-    df = pd.DataFrame(data=np.array(resultsAvg))
-    _, columns = df.shape
-    results = []
-    for j in range(columns):
-        epoch_results = [np.min(df[j]), np.max(df[j]), np.average(df[j])]
-        results.append(epoch_results)
-    df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
-    observer.plot_chart(df)
+    conf_dir = directory + "/conf"
+    for r, d, f in os.walk(conf_dir):
+        f.sort()
+        for file in f:
+            resultsMin = []
+            resultsMax = []
+            resultsAvg = []
+            results = []
+            print(file)
+            observer = Observer(conf_dir + "/" + file)
+            for i in range(runs):
+                population = Population(conf_dir + "/" + file)
+                population.run()
+                minimal, avg, maximal = population.getSimulationResults()
+                resultsMax.append(maximal)
+                resultsMin.append(minimal)
+                resultsAvg.append(avg)
+
+            dfAvg = pd.DataFrame(data=np.array(resultsAvg))
+            dfMax = pd.DataFrame(data=np.array(resultsMax))
+            dfMin = pd.DataFrame(data=np.array(resultsMin))
+            _, columns = dfAvg.shape
+            for j in range(columns):
+                epoch_results = [np.min(dfMin[j]), np.max(dfMax[j]), np.average(dfAvg[j])]
+                results.append(epoch_results)
+            df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
+            print(df.shape)
+            observer.plot_chart(df)
+            df = None
 
 
 if __name__ == '__main__':
