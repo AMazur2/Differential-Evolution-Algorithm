16c48919ca92572de278b7a1a50ab1321202b029
aksek <blackbirdcu@gmail.com>
2021-12-11 18:04:59 +0100

    normalized ECDF observer

--- main.py
+++ main.py
@@ -1,54 +1,7 @@
-from src.Population import Population
-from src.Observer import Observer
-from src.ObserverECDF import ObserverECDF
-import numpy as np
-import pandas as pd
 import os
-from datetime import datetime
 
-
-#def main():
-#    now = datetime.now()
-#    current_time = now.strftime("%H:%M:%S")
-#    print("Current Time =", current_time)
-#
-#    runs = 10
-#    directory = os.getcwd()
-#    conf_dir = directory + "/conf"
-#    for r, d, f in os.walk(conf_dir):
-#        f.sort()
-#        for file in f:
-#            resultsMin = []
-#            resultsMax = []
-#            resultsAvg = []
-#            results = []
-#            print(file)
-#            observer = Observer(conf_dir + "/" + file)
-#            observerECDF = ObserverECDF(conf_dir + "/" + file)
-#            for i in range(runs):
-#                population = Population(conf_dir + "/" + file)
-#                population.run()
-#                minimal, avg, maximal = population.getSimulationResults()
-#                resultsMax.append(maximal)
-#                resultsMin.append(minimal)
-#                resultsAvg.append(avg)
-#
-#            dfAvg = pd.DataFrame(data=np.array(resultsAvg))
-#            dfMax = pd.DataFrame(data=np.array(resultsMax))
-#            dfMin = pd.DataFrame(data=np.array(resultsMin))
-#
-#            _, columns = dfAvg.shape
-#            for j in range(columns):
-#                epoch_results = [np.min(dfMin[j]), np.max(dfMax[j]), np.average(dfAvg[j])]
-#                results.append(epoch_results)
-#            df = pd.DataFrame(data=np.array(results), columns=["min", "max", "average"])
-#            observer.plot_chart(df)
-#
-#            observerECDF.plot_chart(resultsMin)
-#
-#    now = datetime.now()
-#    current_time = now.strftime("%H:%M:%S")
-#    print("Current Time =", current_time)
+from src.ObserverECDF import ObserverECDF
+from src.Population import Population
 
 
 def main():
--- src/ObserverECDF.py
+++ src/ObserverECDF.py
@@ -11,6 +11,7 @@ class ObserverECDF:
             config = json.load(configuration)
             self.epochs = config['epochs']
             self.dim = config['dimension']
+            self.resolution = config['output_resolution']
             mutationConfig = config['mutator']
             self.mutationF = mutationConfig['mutation_factor']
             self.hyper = mutationConfig['hypermutation_factor']
@@ -18,19 +19,43 @@ class ObserverECDF:
             self.populationSize = generatorConfig['size']
 
     def plot_chart(self, all_min_results):
-        firstECDF = ECDF(all_min_results[0])
-        x = np.array(firstECDF.x)
-        y = np.array(firstECDF.y)
 
-        for i in range(1, len(all_min_results)):
-            y += ECDF(all_min_results[i]).y
+        option_counter = 0
+        for hypermutation_option_results in all_min_results:
+            x_min = float('inf')
+            x_max = float('-inf')
+            ECDFs = []
+            for i in range(1, len(hypermutation_option_results)):
+                current_ECDF = ECDF(hypermutation_option_results[i])
+                current_x = current_ECDF.x
 
-        y /= len(all_min_results)
+                first_x = current_x[1]
+                if first_x < x_min and first_x != float('-inf'):
+                    x_min = first_x
 
-        plt.plot(x, y)
+                last_x = current_x[len(current_x) - 1]
+                if last_x > x_max and last_x != float('inf'):
+                    x_max = last_x
+                ECDFs.append(current_ECDF)
+
+            x = np.arange(x_min, x_max, (x_max - x_min)/self.resolution)
+            y = np.zeros(self.resolution)
+
+            for current_ECDF in ECDFs:
+                i = 0
+                for current_x in x:
+                    y[i] += current_ECDF(current_x)
+                    i += 1
+
+            y /= len(hypermutation_option_results)
+
+            plt.step(x, y, label=("Hypermutation on" if option_counter == 0 else "Hypermutation off")) #
+            option_counter += 1
 
         plt.xlabel("results")
         plt.ylabel("proportion of function-target pairs")
+        plt.title("Dimension:" + str(self.dim) + " Hypermutation factor:" + str(self.hyper) + " Mutation factor:" + str(self.mutationF))
+        plt.legend()
 
         name = ("e" + str(self.epochs) + "-p" + str(self.populationSize) + "-d" + str(self.dim) +
                 "-m" + str(self.mutationF) + "-h" + str(self.hyper) + "ECDF.png")
